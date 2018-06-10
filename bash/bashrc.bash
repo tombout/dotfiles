0d05ccb313129ad6a51d5046516defc5b041f2fe
Thomas Schmitz <thomas.schmitz80@gmail.com>
2018-06-11 00:02:41 +0200

    Use uppercase variable names

--- bash/bashrc.bash
+++ bash/bashrc.bash
@@ -3,12 +3,7 @@
 # If not running interactively, don't do anything
 [ -z "$PS1" ] && return
 
-MACHINE="$(uname -s)"
-
-# Remember that this script is sourced from ~/.bashrc
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-INCLUDES="$SCRIPT_DIR/includes"
-ALIASES="$SCRIPT_DIR/aliases"
+machine="$(uname -s)"
 
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
@@ -19,8 +14,13 @@ shopt -s checkwinsize
 # Kill ssh-agent on exit
 trap "kill $SSH_AGENT_PID" SIGHUP
 
-[[ -f "$INCLUDES/prompt.bash" ]] && source "$INCLUDES/prompt.bash"
-[[ -f "$INCLUDES/history.bash" ]] && source "$INCLUDES/history.bash"
+# Remember that this script is sourced from ~/.bashrc
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+includes="$script_dir/includes"
+aliases="$script_dir/aliases"
+
+[[ -f "$includes/prompt.bash" ]] && source "$includes/prompt.bash"
+[[ -f "$includes/history.bash" ]] && source "$includes/history.bash"
 
 # Source all aliases
-[[ -d "$ALIASES" ]] && for f in "$ALIASES"/*; do source "$f"; done
+[[ -d "$aliases" ]] && for f in "$aliases"/*; do source "$f"; done
