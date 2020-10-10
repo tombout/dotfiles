8e97fd9d93c4d2ecd5ea0bf661427e8e08c45785
Thomas Schmitz <dev@tschmitz.de>
2020-10-11 01:28:02 +0200

    Clean up aliases

--- bash/aliases/docker.aliases.bash
+++ bash/aliases/docker.aliases.bash
@@ -1,4 +1,4 @@
 alias docker-remove-untagged-images='docker rmi $(docker images -q --filter "dangling=true")'
 alias docker-remove-all-images='docker rmi $(docker images -q)'
 alias docker-remove-stopped-container='docker rm $(docker ps -a -q)'
-alias dimages='docker images'
+
--- bash/bashrc.bash
+++ bash/bashrc.bash
@@ -1,7 +1,9 @@
 #@IgnoreInspection BashAddShebang
 
 # Use same language on all systems
-export LANG=en_US.uft8
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 
 # If not running interactively, don't do anything
 [ -z "$PS1" ] && return
